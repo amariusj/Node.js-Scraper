c189f7dbd373957098d9178f8f3951828bac8e3f
Amarius Jones <amariusj@gmail.com>
2018-09-01 14:57:56 -0400

    Add files via upload

--- Workspaces/index.js
+++ Workspaces/index.js
@@ -1,12 +1,25 @@
 var request = require('request');
 var cheerio = require('cheerio');
-var CSV = require('csv-string');
+var csv = require('csv')
 var fs = require('fs');
 var dir = './data';
 
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
 }
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+
+var yyyy = today.getFullYear();
+if(dd<10){
+    dd='0'+dd;
+}
+if(mm<10){
+    mm='0'+mm;
+}
+var today = mm+'_'+dd+'_'+yyyy;
 
 var item = [];
 var shirtsUrl = "http://shirts4mike.com/shirts.php";
@@ -15,6 +28,11 @@ request(shirtsUrl, function(error, response, html) {
   if (!error) {
     var $ = cheerio.load(html);
     var product = $('.products').children();
+    var headers = `
+    Title, Price, Image Url, Product Url, Date Published
+    `;
+
+    item.push(headers);
 
     product.each(function(index) {
       var productUrl = `http://shirts4mike.com/${product.eq(index).children().attr('href')}`;
@@ -23,25 +41,167 @@ request(shirtsUrl, function(error, response, html) {
           var product$ = cheerio.load(html);
           let price = product$('.price').text();
           let imgUrl = product$('.shirt-picture').find('img').attr('src');
-          let title = product$('.shirt-picture').find('img').attr('alt');
+          if (productUrl === `http://shirts4mike.com/${product.eq(0).children().attr('href')}`) {
+            let title = 'Logo Shirt Red';
+            console.log(title);
+            var itemCollective = `
+            ${title}, ${price}, ${imgUrl}, ${productUrl}, ${Date()}
+          `;
 
-          var itemCollective = {
-            title: title,
-            price: price,
-            ImageUrl: imgUrl,
-            url: productUrl,
-            time: Date()
-          }
-
-          item.push(CSV.stringify(itemCollective));
+          item.push(itemCollective);
           console.log(itemCollective);
-          fs.writeFile('data/8_29_2018.csv', item, function (err) {
+
+          fs.writeFile('data/' + today + '.csv', item, function (err) {
             if (err) {
               throw err;
               } else {
               console.log('It worked!');
               }
           });
+          } else
+          if (productUrl === `http://shirts4mike.com/${product.eq(3).children().attr('href')}`) {
+            let title = 'Logo Shirt Green';
+            console.log(title);
+            var itemCollective = `
+              ${title}, ${price}, ${imgUrl}, ${productUrl}, ${Date()}
+            `;
+
+            item.push(itemCollective);
+            console.log(itemCollective);
+
+            fs.writeFile('data/' + today + '.csv', item, function (err) {
+              if (err) {
+                throw err;
+                } else {
+                console.log('It worked!');
+                }
+            });
+          } else
+          if (productUrl === `http://shirts4mike.com/${product.eq(6).children().attr('href')}`) {
+            let title = 'Logo Shirt Teal';
+            console.log(title);
+            var itemCollective = `
+              ${title}, ${price}, ${imgUrl}, ${productUrl}, ${Date()}
+            `;
+
+            item.push(itemCollective);
+            console.log(itemCollective);
+
+            fs.writeFile('data/' + today + '.csv', item, function (err) {
+              if (err) {
+                throw err;
+                } else {
+                console.log('It worked!');
+                }
+            });
+          } else
+          if (productUrl === `http://shirts4mike.com/${product.eq(5).children().attr('href')}`) {
+            let title = 'Logo Shirt Gray';
+            console.log(title);
+            var itemCollective = `
+              ${title}, ${price}, ${imgUrl}, ${productUrl}, ${Date()}
+            `;
+
+            item.push(itemCollective);
+            console.log(itemCollective);
+
+            fs.writeFile('data/' + today + '.csv', item, function (err) {
+              if (err) {
+                throw err;
+                } else {
+                console.log('It worked!');
+                }
+            });
+          } else
+          if (productUrl === `http://shirts4mike.com/${product.eq(1).children().attr('href')}`) {
+            let title = 'Mike the Frog Shirt Black';
+            console.log(title);
+            var itemCollective = `
+              ${title}, ${price}, ${imgUrl}, ${productUrl}, ${Date()}
+            `;
+
+            item.push(itemCollective);
+            console.log(itemCollective);
+
+            fs.writeFile('data/' + today + '.csv', item, function (err) {
+              if (err) {
+                throw err;
+                } else {
+                console.log('It worked!');
+                }
+            });
+          }
+          else
+          if (productUrl === `http://shirts4mike.com/${product.eq(2).children().attr('href')}`) {
+            let title = 'Mike the Frog Shirt Blue';
+            console.log(title);
+            var itemCollective = `
+              ${title}, ${price}, ${imgUrl}, ${productUrl}, ${Date()}
+            `;
+
+            item.push(itemCollective);
+            console.log(itemCollective);
+
+            fs.writeFile('data/' + today + '.csv', item, function (err) {
+              if (err) {
+                throw err;
+                } else {
+                console.log('It worked!');
+                }
+            });
+          }
+          else
+          if (productUrl === `http://shirts4mike.com/${product.eq(4).children().attr('href')}`) {
+            let title = 'Mike the Frog Shirt Yellow';
+            console.log(title);
+            var itemCollective = `
+              ${title}, ${price}, ${imgUrl}, ${productUrl}, ${Date()}
+            `;
+
+            item.push(itemCollective);
+            console.log(itemCollective);
+
+            fs.writeFile('data/' + today + '.csv', item, function (err) {
+              if (err) {
+                throw err;
+                } else {
+                console.log('It worked!');
+                }
+            });
+          }
+          else
+          if (productUrl === `http://shirts4mike.com/${product.eq(7).children().attr('href')}`) {
+            let title = 'Mike the Frog Shirt Orange';
+            console.log(title);
+            var itemCollective = `
+              ${title}, ${price}, ${imgUrl}, ${productUrl}, ${Date()}
+            `;
+
+            item.push(itemCollective);
+            console.log(itemCollective);
+
+            fs.writeFile('data/' + today + '.csv', item, function (err) {
+              if (err) {
+                throw err;
+                } else {
+                console.log('It worked!');
+                }
+            });
+          }
+          // var itemCollective = `
+          //   ${title}, ${price}, ${imgUrl}, ${productUrl}, ${Date()}
+          // `;
+
+          // item.push(itemCollective);
+          // console.log(itemCollective);
+          //
+          // fs.writeFile('data/' + today + '.csv', item, function (err) {
+          //   if (err) {
+          //     throw err;
+          //     } else {
+          //     console.log('It worked!');
+          //     }
+          // });
         }
       });
      });
